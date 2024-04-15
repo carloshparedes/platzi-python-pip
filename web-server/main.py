6af87859b7b71a304b6699e4948d21ab7c336a80
Carlos Hernandez <carloshparedes@gmail.com>
2024-04-15 16:01:25 +0200

    Add API endpoints for getting a list of items and contacts in main.py

--- web-server/main.py
+++ web-server/main.py
@@ -3,6 +3,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/")
+def get_list():
+    return ["item1", "item2", "item3"]
+
+@app.get("/contact")
+def get_list():
+    return [{"name" : "Platzi"}, "contact1", "contact2", "contact3"]
 
 
 def run():
